d0d186d1bc0cda36169b94662f1af57cadf780db
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-08-20 14:46:09 +1200

    Placeholder data set (y = sin(x) + noise)

--- Code/main.cpp
+++ Code/main.cpp
@@ -9,7 +9,7 @@ using namespace DNest3;
 int main(int argc, char** argv)
 {
 	// Load the data
-	Data::get_instance().load("fake_data.txt");
+	Data::get_instance().load("data.txt");
 
 	MTSampler<MyModel> sampler = setup_mt<MyModel>(argc, argv);
 	sampler.run();
