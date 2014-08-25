10d77616c9ad52fe15250af0c8492db2b9dff101
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-08-26 11:25:28 +1200

    Fixed little mistake

--- Code/display.py
+++ Code/display.py
@@ -7,7 +7,7 @@ log_periods = array([])
 
 for i in xrange(0, posterior_sample.shape[0]):
   N = posterior_sample[i, 7]
-  log_periods = hstack([log_periods, posterior_sample[i, 7:7+N]])
+  log_periods = hstack([log_periods, posterior_sample[i, 8:8+N]])
 
 hist(log_periods, 200)
 xlabel(r'$\ln$(Period)', fontsize=20)
