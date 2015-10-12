238cc0873ed4ac4589583d95f600fd0c2646b797
Brendon Brewer <brendon.brewer@gmail.com>
2015-10-12 18:24:35 +0100

    Transferring some of the Julia code over

--- Code/StateSpace.cpp
+++ Code/StateSpace.cpp
@@ -10,7 +10,7 @@ using namespace DNest3;
 using namespace Eigen;
 
 StateSpace::StateSpace()
-:objects(3, 30, false, MyDistribution())
+:objects(3, 1, true, MyDistribution())
 {
 
 }
@@ -47,12 +47,39 @@ double StateSpace::logLikelihood() const
 	// Get the modes
 	const vector< vector<double> >& components = objects.get_components();
 
-	// First data point
-	double var0 = 0.;
-	for(size_t i=0; i<components.size(); i++)
-		var0 += pow(components[i][1], 2);
-	var0 += extra_sigma*extra_sigma;
-	logL += -0.5*sqrt(2*M_PI*var0) -0.5*pow(Y[0], 2)/var0;
+	// Mode parameters
+	double omega0 = 2*M_PI*exp(-components[0][0]);
+	double A = components[0][1];
+	double tau = components[0][2]*exp(components[0][0]);
+	double D = A*A*omega0*omega0*tau;
+	double omega = sqrt(omega0*omega0 - 1.0/(4*tau*tau));
+
+	// The matrices M, I, and J
+	MatrixXd M(2, 2), I(2, 2), J(2, 2);
+	M<<0.0, -1.0, omega0*omega0, 1.0/tau;
+	I<<1.0, 0.0, 0.0, 1.0;
+	J<<1.0/(2*omega*tau), 1.0/omega, -omega0*omega0/omega, -1.0/(2*omega*tau);
+
+	// Probability distribution for the signal (x, v)
+	// is gaussian. Its mean and covariance matrix are mu and C
+	VectorXd mu = VectorXd::Zero(2);
+	MatrixXd C(2, 2);
+
+	// For the first point, Dt=Infinity
+	C(0, 0) = D/(4*pow(omega*omega0, 2)*pow(tau, 3))*(4*pow(omega*tau, 2));
+	C(0, 1) = 0.;
+	C(1, 0) = 0.;
+	C(1, 1) = D/(4*pow(omega, 2)*pow(tau, 3))*4*pow(omega*tau, 2);
+
+	double var;
+	for(int i=0; i<Y.size(); i++)
+	{
+		// Probability distribution for the data point given the signal
+		var = C(0, 0) + pow(extra_sigma, 2);
+		logL += -0.5*log(2*M_PI*var) - 0.5*pow(Y[i], 2)/var;
+		
+
+	}
 
 	return logL;
 }
