f6d64b1c5db3b5ff96da2ece18e5c1d09e19b721
Brendon Brewer <brendon.brewer@gmail.com>
2015-10-12 13:34:15 +0100

    Enter matrices properly

--- Code/Sandpit/analytic.jl
+++ Code/Sandpit/analytic.jl
@@ -12,8 +12,8 @@ tau = 5.0
 # Cyclic frequency of the damped oscillator
 omega = sqrt(omega0^2 - 1.0/(4.0*tau^2))
 
-# The matrix M
-M = [[0.0, -1.0]; [omega0^2, 1.0/tau]]
-
-println(M)
+# The matrices M, I, and J
+M = [0.0 -1.0; omega0^2 1.0/tau]
+I = eye(2)
+J = [1.0/(2.0*omega*tau) 1.0/omega; -omega0^2/omega -1.0/(2.0*omega*tau)]
 
