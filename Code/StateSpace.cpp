c8a40a8f6fc6923ba6b8da9edc2a78694316cfce
Brendon Brewer <brendon.brewer@gmail.com>
2015-10-17 19:31:45 +0100

    Generalise to N modes

--- Code/StateSpace.cpp
+++ Code/StateSpace.cpp
@@ -4,6 +4,7 @@
 #include "Data.h"
 #include <cmath>
 #include <Eigen/Dense>
+#include <unsupported/Eigen/MatrixFunctions>
 
 using namespace std;
 using namespace DNest3;
@@ -48,44 +49,74 @@ double StateSpace::logLikelihood() const
 
 	// Get the modes
 	const vector< vector<double> >& components = objects.get_components();
+	int N = components.size();
 
 	// Mode parameters
-	double omega0 = 2*M_PI*exp(-components[0][0]);
-	double A = components[0][1];
-	double tau = components[0][2]*exp(components[0][0]);
-	double D = A*A*omega0*omega0*tau;
-	double omega = sqrt(omega0*omega0 - 1.0/(4*tau*tau));
-
-	// The matrices M, I, and J
-	MatrixXd M(2, 2), I(2, 2), J(2, 2);
-	M<<0.0, -1.0, omega0*omega0, 1.0/tau;
-	I<<1.0, 0.0, 0.0, 1.0;
-	J<<1.0/(2*omega*tau), 1.0/omega, -omega0*omega0/omega, -1.0/(2*omega*tau);
+	vector<double> omega0(N);
+	vector<double> A(N);
+	vector<double> tau(N);
+	vector<double> D(N);
+	vector<double> omega(N);
+	for(int i=0; i<N; i++)
+	{
+		omega0[i] = 2*M_PI*exp(-components[i][0]);
+		A[i] = components[i][1];
+		tau[i] = components[i][2]*exp(components[i][0]);
+		D[i] = A[i]*A[i]*omega0[i]*omega0[i]*tau[i];
+		omega[i] = sqrt(omega0[i]*omega0[i] - 1.0/(4*tau[i]*tau[i]));
+	}
 
 	// State of knowledge of signal
-	VectorXd mu = VectorXd::Zero(2);
-	MatrixXd C(2, 2);
-	C(0, 0) = D/pow(omega0, 2)/tau;
-	C(0, 1) = 0.;
-	C(1, 0) = 0.;
-	C(1, 1) = D/tau;
+	// (y1, v1, y2, v2, ..., yN, vN)
+	VectorXd mu = VectorXd::Zero(2*N);
+	MatrixXd C = MatrixXd::Zero(2*N, 2*N);
+
+	// The matrices M, I, and J
+	MatrixXd M = MatrixXd::Zero(2*N, 2*N);
+	MatrixXd I = MatrixXd::Identity(2*N, 2*N);
+	MatrixXd J = MatrixXd::Zero(2*N, 2*N);
+	for(int i=0; i<N; i++)
+	{
+		M(2*i, 2*i+1) = -1.;
+		M(2*i+1, 2*i) = pow(omega0[i], 2);
+		M(2*i+1, 2*i+1) = 1./tau[i];
+
+		J(2*i, 2*i) = 1./(2*omega[i]*tau[i]);
+		J(2*i, 2*i+1) = -1./omega[i];
+		J(2*i+1, 2*i) = -pow(omega0[i], 2)/omega[i];
+		J(2*i+1, 2*i+1) = -1./(2*omega[i]*tau[i]);
+
+		C(2*i, 2*i) = D[i]/pow(omega0[i], 2)/tau[i];
+		C(2*i+1, 2*i+1) = D[i]/tau[i];
+	}
 
 	// Declare stuff
-	double Dt, var;
-	MatrixXd C1inv(2, 2), C2inv(2, 2), C3(2, 2), mexp(2, 2);
-	VectorXd mu2(2);
+	double Dt, mean, var;
+	MatrixXd C1inv(2*N, 2*N), C2inv(2*N, 2*N), C3(2*N, 2*N), mexp(2*N, 2*N);
+	VectorXd mu2(2*N);
 
 	for(int i=0; i<Y.size(); i++)
 	{
 		// Evaluate probability distribution for the data point
-		var = C(0, 0) + pow(sig[i], 2);//pow(extra_sigma, 2);
-		logL += -0.5*log(2*M_PI*var) - 0.5*pow(Y[i] - mu[0], 2)/var;
+		mean = 0.;
+		var = 0.;
+		for(int j=0; j<N; j++)
+		{
+			mean += mu(2*j);
+			var += C(2*j, 2*j);
+		}
+		var += pow(sig[i], 2);
+		logL += -0.5*log(2*M_PI*var) - 0.5*pow(Y[i] - mean, 2)/var;
 
 		// Update knowledge of signal at current time
 		C1inv = C.inverse();
-		C2inv<<(1.0/pow(sig[i], 2)), 0.0, 0.0, 0.0;
+		C2inv = MatrixXd::Zero(2*N, 2*N);
+		for(int j=0; j<N; j++)
+			C2inv(2*j, 2*j) = pow(sig[i], -2);
 		C3 = (C1inv + C2inv).inverse();
-		mu2<<Y[i], 0.;
+		mu2 = VectorXd::Ones(2*N);
+		for(int j=0; j<N; j++)
+			mu2(2*j) = Y[i]/N;
 		mu = C3*C1inv*mu + C3*C2inv*mu2;
 		C = C3;
 
@@ -95,17 +126,21 @@ double StateSpace::logLikelihood() const
 			Dt = t[i+1] - t[i];
 
 			// Just reuse C3, don't need a new matrix
-			C3(0, 0) = D/(4*pow(omega*omega0, 2)*pow(tau, 3))*
-						(4*pow(omega*tau, 2) + exp(-Dt/tau)*
-						(cos(2*omega*Dt) - 2*omega*tau*sin(2*omega*Dt) -
-								 4*pow(omega0*tau, 2)));
-			C3(0, 1) = D/pow(omega, 2)/pow(tau, 2)*exp(-Dt/tau)*pow(sin(omega*Dt), 2);
-			C3(1, 0) = C3(0, 1);
-			C3(1, 1) = D/(4*pow(omega, 2)*pow(tau, 3))*
-						(4*pow(omega*tau, 2) + exp(-Dt/tau)*
-						(cos(2*omega*Dt) + 2*omega*tau*sin(2*omega*Dt) - 4*pow(omega0*tau, 2)));
+			C3 = MatrixXd::Zero(2*N, 2*N);
+			for(int j=0; j<N; j++)
+			{
+				C3(2*j, 2*j) = D[j]/(4*pow(omega[j]*omega0[j], 2)*pow(tau[j], 3))*
+							(4*pow(omega[j]*tau[j], 2) + exp(-Dt/tau[j])*
+							(cos(2*omega[j]*Dt) - 2*omega[j]*tau[j]*sin(2*omega[j]*Dt) -
+									 4*pow(omega0[j]*tau[j], 2)));
+				C3(2*j, 2*j+1) = D[j]/pow(omega[j], 2)/pow(tau[j], 2)*exp(-Dt/tau[j])*pow(sin(omega[j]*Dt), 2);
+				C3(2*j+1, 2*j) = C3(0, 1);
+				C3(2*j+1, 2*j+1) = D[j]/(4*pow(omega[j], 2)*pow(tau[j], 3))*
+							(4*pow(omega[j]*tau[j], 2) + exp(-Dt/tau[j])*
+							(cos(2*omega[j]*Dt) + 2*omega[j]*tau[j]*sin(2*omega[j]*Dt) - 4*pow(omega0[j]*tau[j], 2)));
+			}
 
-			mexp = exp(-0.5*Dt/tau)*(cos(omega*Dt)*I + sin(omega*Dt)*J);
+			mexp = (-Dt*M).exp();
 			mu = mexp*mu;
 			C = mexp*C*mexp.transpose() + C3;
 		}
