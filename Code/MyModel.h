60311c5c5cb8f00843c2360062a13b5029a661ce
Brendon Brewer <brendon.brewer@gmail.com>
2016-05-04 16:26:47 +1000

    Template

--- Code/MyModel.h
+++ Code/MyModel.h
@@ -1,37 +1,27 @@
-#ifndef _MyModel_
-#define _MyModel_
+#ifndef Oscillations2_MyModel
+#define Oscillations2_MyModel
 
-#include "Model.h"
-#include <vector>
-#include <RJObject.h>
-#include "MyDistribution.h"
-#include <Eigen/Dense>
-#include <Eigen/Cholesky>
-#include <HODLR_Matrix.hpp>
-#include <HODLR_Tree.hpp>
+#include "DNest4/code/DNest4.h"
+#include "MyConditionalPrior.h"
+#include <ostream>
 
-class MyModel:public DNest3::Model
+class MyModel
 {
-	friend class HODLRSolverMatrix;
-
 	private:
-		RJObject<MyDistribution> objects;
 
-		double extra_sigma;
-
-		double calculate_C(int i, int j) const;
 
 	public:
+		// Constructor only gives size of params
 		MyModel();
 
 		// Generate the point from the prior
-		void fromPrior();
+		void from_prior(DNest4::RNG& rng);
 
 		// Metropolis-Hastings proposals
-		double perturb();
+		double perturb(DNest4::RNG& rng);
 
 		// Likelihood function
-		double logLikelihood() const;
+		double log_likelihood() const;
 
 		// Print to stream
 		void print(std::ostream& out) const;
@@ -39,25 +29,6 @@ class MyModel:public DNest3::Model
 		// Return string with column information
 		std::string description() const;
 };
-
-
-class HODLRSolverMatrix : public HODLR_Matrix
-{
-	private:
-		const MyModel& model;
-
-public:
-    HODLRSolverMatrix (const MyModel& model)
-        : model(model)
-    {
-    }
-
-    double get_Matrix_Entry (const unsigned i, const unsigned j) {
-        return model.calculate_C(i, j);
-    };
-
-};
-
 
 #endif
 
